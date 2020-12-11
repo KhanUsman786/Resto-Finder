fa8c47b029674310384db30f7384278660e39e16
Usman Rashid Khan <khanusman786@live.com>
2020-12-11 14:50:25 +0530

    Add files via upload

---
+++ src/util/Yelp.js
@@ -0,0 +1,30 @@
+const apiKey = 'TuF4W5si9pZRU330oKwgkP4TPkYHqiGhgm5vylK24zuibHZ_pwnMbfS6FAOlskgVDe8A4r-Bruu1DUMZGmwPXylqaCV-ih8IlMGOw9NwVDqKxPoYxlRGrKtoHDp3X3Yx'; // Insert API key here.
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => ({
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }));
+      }
+    });
+  }
+};
+
+export default Yelp;
